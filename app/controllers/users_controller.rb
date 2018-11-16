b1fa92fae6706d212359fff6441226066632d8cf
ARATA <newbluelight1031@gmail.com>
2018-11-16 22:56:56 +0000

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+    
+  def index
+      @users = User.all
+  end
+  
+  def new
+  end
+  
+  def create
+      @user = User.new
+      @user.name = params[:name]
+      @user.user_image = "original.jpg"
+      @user.save
+          redirect_to("/users")
+  end
+  
+  def  edit
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def update
+     @user = User.find_by(id: params[:id])
+     @user.name = params[:name]
+     @user.user_image = :"#{@user.id}.jpg"
+     if @user.save
+       image = params[:user_image]
+       File.binwrite("app/assets/images/#{@user.id}.jpg", image.read)
+       redirect_to("/users")
+     else
+       redirect_to("/users")
+     end
+  end
+  
+end
